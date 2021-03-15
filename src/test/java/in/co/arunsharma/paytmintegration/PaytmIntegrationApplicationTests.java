648e6ed06e2df63bcd98817fbaf5dd33971dd3f3
arun kumar <arun30687@gmail.com>
2021-03-15 06:21:03 +0530

    initial commit

---
+++ src/test/java/in/co/arunsharma/paytmintegration/PaytmIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.arunsharma.paytmintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytmIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
